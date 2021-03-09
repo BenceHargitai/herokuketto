43a7d089002ac8ee0b32636ac3fe36be3b40bea1
BenceHargitai <beni.hargitai@gmail.com>
2021-03-09 11:18:03 +0100

    commit

--- lista/urls.py
+++ lista/urls.py
@@ -27,6 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('torles/', torles),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
 ]
